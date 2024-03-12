4f03f85037822ef99f5e527de201d4c8538fc820
costin_branzas <costin.branzas@qubiz.com>
2024-03-12 16:10:27 +0200

    added functioning observable with map operator

--- src/app/rxjs-module/map-component/map.component.ts
+++ src/app/rxjs-module/map-component/map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer, Subscriber } from 'rxjs';
 
 
 @Component({
@@ -14,9 +15,47 @@ import { Component } from '@angular/core';
 export class MapComponent {
   title = "MapComponentTitle";
   
+  
+
   constructor() {}
 
   doStuff() {
     console.log("Map component doing stuff");
+
+    let observer1 = new Observer1<string>();
+
+    let observable1 = new Observable<string>(this.observableBehaviour).map(this.mapFunctionality);
+
+    observable1.subscribe(observer1);
+
+  }
+
+  observableBehaviour(subscriber:Subscriber<any>) {
+    for (let i=1; i<=10; i++)
+    {
+      subscriber.next(i.toString());
+    }
+    subscriber.complete();
+  }
+
+  mapFunctionality(data: string) {
+    return data + " with some stuff added to it by map operator";
+  }
+
+}
+
+class Observer1<T> implements Observer<T> {
+  next(x: T): void {
+    //do shit with X
+    console.log(x);
+  }
+
+  error(error: any): void {
+    console.error(error);
+  }
+
+  complete(): void {
+    //do whatever is needed if observable ends
+    console.log("done");
   }
 }
