0f39e7cae09cee7d8a4249cef90c7a904376e3a5
costin_branzas <costin.branzas@qubiz.com>
2024-03-29 16:30:05 +0200

    added some documented comment for directives + change detection issue

--- src/app/directives-module/components/directives-and-change-detection.component.ts
+++ src/app/directives-module/components/directives-and-change-detection.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   template: 
   `<div style = "border:1px solid black; padding: 10px;">
   
-    <div *ngIf="true"> <!-- if this div does not have ngIf on it same as for dummy-directive2, then ngAfterViewInit will NOT get called (reproducing the bug on darwin) -->
+    <!-- Observed Behaviour in Angular4: if 1 directive is used in a block with a structural directive AND calls on change detection, this will stop ngAfterViewInit being called on the other directive  -->
+    <div> <!-- if this div does not have ngIf on it same as for dummy-directive2, then ngAfterViewInit will NOT get called (reproducing the bug on darwin) -->
       <p dummy-directive1>dummy directive 1</p> <br/>  
     </div>
     
