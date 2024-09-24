060e34b3887b5777e248b9caf989025567c9c104
costin_branzas <costin.branzas@qubiz.com>
2024-09-24 10:34:58 +0300

    adding a comment and disabling circular dependencies warning

--- src/app/circular-dep/components/circular-dep2.component.ts
+++ src/app/circular-dep/components/circular-dep2.component.ts
@@ -6,9 +6,11 @@ import { Component, Input } from '@angular/core';
   template: 
   `<div style = "border:1px solid black; padding: 10px;">
     <p>{{title}}<p>
+    <!-- <p>if the following section is left uncommented, we get a circular dep warning at build, even if the ngIf prevents it from happening at run time</p>
     <div *ngIf="enableCallbackToCircularDep1">
         <circular-dep1></circular-dep1>
     </div>
+    -->
   </div>`
 })
 export class CircularDep2 {
