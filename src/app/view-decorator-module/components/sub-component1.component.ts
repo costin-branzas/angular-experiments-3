2e2f37653e2166db6ac1bd2c2be18ed549aa2754
costin_branzas <costin.branzas@qubiz.com>
2025-01-15 10:39:06 +0200

    view child experiment

--- src/app/view-decorator-module/components/sub-component1.component.ts
+++ src/app/view-decorator-module/components/sub-component1.component.ts
@@ -1,17 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'sub-component1',
   template: 
   `<div style = "border:1px solid black; padding: 10px;">
     <p>{{title}}<p>
+    <p>{{inputValue}}</p>
     
   </div>`
 })
 export class SubComponent1 {
   title = "sub-component1";
 
+  @Input() inputValue: number;
+
   constructor() { }
+
+  ngOnChanges(changes) {
+    console.log("sub-component1 ngOnChanges");
+    console.log("changes = ", changes);
+  }
 
 }
--- src/app/view-decorator-module/view-decorator.module.ts
+++ src/app/view-decorator-module/view-decorator.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { ViewChildExperiment1 } from './components/view-child-experiment1.component';
 import { SubComponent1 } from './components/sub-component1.component';
+import { SubComponent2 } from './components/sub-component2.component';
 
 @NgModule({
     imports: [
@@ -23,7 +24,8 @@ import { SubComponent1 } from './components/sub-component1.component';
     exports: [RouterModule],
     declarations: [
         ViewChildExperiment1,
-        SubComponent1
+        SubComponent1,
+        SubComponent2
     ],
 })
 export class ViewDecoratorModule {}
