49aa0354743a6d9cd15e5a30efb346809c6e4db2
costin_branzas <costin.branzas@qubiz.com>
2025-01-22 16:02:01 +0200

    replicated pattern from Darwin

--- src/app/view-decorator-module/components/sub-component.component.ts
+++ src/app/view-decorator-module/components/sub-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -25,13 +26,19 @@ export class SubComponent {
   ngOnChanges(changes: SimpleChanges) {
     console.log("**** **** sub-component ngOnChanges");
     console.log("changes = ", changes);
-    console.log("targetReference = ", this.targetReference);
+    console.log("targetReference = ", this.targetReference);    
   }
   
   ngOnInit() {
     console.log("**** **** sub-component ngOnInit");
     console.log("targetReference = ", this.targetReference);
 
+    //he observable below does not trigger change detection, and the next handler runs as soon as the observable is subscribed, however the aMethod body (that is wraped in setTimeout) will run AFTER change detection
+    of(1).subscribe((data)=>{
+      console.log("**** **** sub-component ngOnInit observable next");
+      console.log("targetReference = ", this.targetReference);
+      this.aMethod();
+    });
   }
 
   ngAfterViewInit() {
@@ -41,6 +48,15 @@ export class SubComponent {
 
   ngAfterViewChecked() {
     console.log("**** **** sub-component ngAfterViewCheked");
+    console.log("targetReference = ", this.targetReference);
+  }
+
+  private aMethod(){
+    setTimeout(() => {
+      console.log("**** **** sub-component aMethod");
+      console.log("targetReference = ", this.targetReference); // because this is in a timeout, by the time it runs, change detection will have finished, no matter the actual timeout value - 0 or an actual number
+    }, 0);
+    console.log("**** **** sub-component aMethod outside setTimeout");
     console.log("targetReference = ", this.targetReference);
   }
 }
